82188089b995f9f53ca6699cbb0539487620dbff
hao940953596 <940953596@qq.com>
2017-03-17 16:33:31 +0800

    first commit

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello World");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
